a4f20999fa20894d1e0d536b1df47d0589e5f002
Torocsik Mark <toro.mark99@gmail.com>
2025-04-23 13:31:21 +0200

    inventory created

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -1,10 +1,24 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Actor {
+    private List<Item> inventory =new ArrayList<>();
+
     public Player(Cell cell) {
         super(cell);
+    }
+
+    public void addToInventory(Item item) {
+        addToInventory(item);
+    }
+
+    public void removeFromInventory(Item item) {
+        inventory.remove(item);
     }
 
     public String getTileName() {
