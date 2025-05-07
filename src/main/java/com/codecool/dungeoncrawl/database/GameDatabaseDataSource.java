b99c43c93042cf6b276259372949caefc622824f
Torocsik Mark <toro.mark99@gmail.com>
2025-05-07 22:45:24 +0200

    data source added

--- src/main/java/com/codecool/dungeoncrawl/database/GameDatabaseDataSource.java
+++ src/main/java/com/codecool/dungeoncrawl/database/GameDatabaseDataSource.java
@@ -2,48 +2,49 @@ package com.codecool.dungeoncrawl.database;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class GameDatabase {
-    String dbName = System.getenv("db_name");
-    String dbUser = System.getenv("dbUserName");
-    String userDbPasswoed = System.getenv("dbPassword");
 
+import java.util.List;
 
+public class GameDatabaseDataSource {
+    private String dbName = System.getenv("dbName");
+    private String dbUser = System.getenv("dbUserName");
+    private String userDbPassword = System.getenv("dbPassword");
 
-    public void getConnection() {
-        try {
-            connect();
-        } catch (SQLException e) {
-            System.out.println("Error connecting to the database" + e.getMessage());
-        }
+    private PGSimpleDataSource dataSource;
+
+    public GameDatabaseDataSource() {
+        initializeDataSource();
     }
 
-    private DataSource connect() throws SQLException {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+    private void initializeDataSource() {
+        dataSource = new PGSimpleDataSource();
         dataSource.setDatabaseName(dbName);
         dataSource.setUser(dbUser);
-        dataSource.setPassword(userDbPasswoed);
-
-        System.out.println("Trying to connect...");
-        dataSource.getConnection().close();
-        System.out.println("Connection OK");
-
-        return dataSource;
+        dataSource.setPassword(userDbPassword);
     }
 
-    public void run() {
-        try {
-            setup();
-        } catch (SQLException throwables) {
-            System.err.println("Could not connect to the database.");
-            return;
+    public Connection connect() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    // Clear all the specified tables (map, item, actor)
+    public void clearAllTables(List<String> tableNames) throws SQLException {
+
+        try (Connection connection = connect()) {
+            for (String table : tableNames) {
+                String sql = "DELETE FROM " + table;
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Error clearing tables", e);
         }
-
-    }
-
-    private void setup() throws SQLException {
-
     }
 }
+
